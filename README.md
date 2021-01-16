8aa099bf1d21c888037ef3fd31f771cef2c3d1dc
Rahul Kashyap <rahulkashyap4041@gmail.com>
2021-01-16 13:45:00 +0530

    Updated README

--- README.md
+++ README.md
@@ -1,16 +1,59 @@
-# konnect
+# Shopify
 
-A new Flutter application.
+* This application is a chatting application built in flutter and backend in firebase.
+
+
+* The key features of the application are:
+1) User Authentication using email/Google/Phone/Facebook
+2) Personal as well as Group chat
+
+
+## Screenshots 
+ 
+| Screens | Screenshots |
+| --------| ----------- |
+| Authentication Screen | <img src="screenshots/login.png" width=400 alt="Authentication Screen">|
+
 
 ## Getting Started
 
-This project is a starting point for a Flutter application.
+#### Section 1: Setting Up for Flutter Development
 
-A few resources to get you started if this is your first Flutter project:
+* [Getting Started: Installing Flutter](https://flutter.dev/docs/get-started/install)
+* [Git for Windows](https://git-scm.com/download/win)
+* [Git for Mac](https://desktop.github.com)
+* [Install Android Studio](https://developer.android.com/studio/)
+* [iOS Setup Instructions](https://flutter.dev/docs/get-started/install/macos#ios-setup)
 
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+#### Section 2: Running this Flutter Project
+**Step 1:**
+Download or clone this repo by using the link below:
 
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+```
+https://github.com/imKashyap/Shopify.git
+```
+**Step 2:**
+Go to project root and execute the following command in console to get the required dependencies: 
+
+``` 
+flutter pub get 
+```
+
+**Step 3:**
+In the project root, execute the following command in console to run the app once you have emulator/device ready: 
+
+``` 
+flutter run
+```
+The application must be up and running in the device.
+
+Do fork and star :star: the repo if you find it appreciable.
+For any queries and suggestions, Conatct me at my mailing address.
+
+
+<!-- | Products Overview Screen | <img src="screenshots/products.png" width=400 alt="Products Overview Screen">|
+| Products Detail Screen | <img src="screenshots/details.png" width=400 alt="Products Detail Screen">|
+| Cart Screen | <img src="screenshots/cart.png" width=400 alt="Cart Screen">|
+| Orders Screen | <img src="screenshots/orders.png" width=400 alt="Orders Screen">|
+| Admin's Products Screen | <img src="screenshots/admin.png" width=400 alt="Admin's Products Screen">|
+| Edit/Add Products Screen | <img src="screenshots/edit.png" width=400 alt="Edit/Add Products Screen">| -->
--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:konnect/utils/colors.dart';
 
 class LoginPage extends StatelessWidget {
+  bool _isLoading = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,14 +10,7 @@ class LoginPage extends StatelessWidget {
       body: SafeArea(
         child: Stack(
           children: [
-            Container(
-              height: 450.0,
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                      alignment: Alignment.topCenter,
-                      fit: BoxFit.contain,
-                      image: AssetImage('assets/images/bg_login.jpg'))),
-            ),
+            _buildBackgroundScreen(),
             Container(
               height: 450.0,
               decoration: BoxDecoration(
@@ -25,72 +19,114 @@ class LoginPage extends StatelessWidget {
                       begin: Alignment.topCenter,
                       end: Alignment.bottomCenter)),
             ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  const SizedBox(
-                    height: 100.0,
-                  ),
-                  Image.asset(
-                    'assets/images/logo.png',
-                    scale: 11,
-                  ),
-                  Text(
-                    'Let those words fly\n to people you want.',
-                    textAlign: TextAlign.center,
-                    style: Theme.of(context).textTheme.headline5.copyWith(
-                        color: Colors.white, fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(
-                    height: 10.0,
-                  ),
-                  Container(
-                    width: double.infinity,
-                    child: RaisedButton(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(18.0),
-                          side: BorderSide(color: Colors.transparent)),
-                      onPressed: () {},
-                      child: Text(
-                        'Sign up Free',
-                        style: Theme.of(context).textTheme.subtitle1.copyWith(
-                              color: Colors.white,
-                            ),
+            _buildInteractionScreen(context),
+            _isLoading
+                ? Container(
+                  width: double.maxFinite,
+                  height: double.maxFinite,
+                    color: Colors.black54,
+                    child: Center(
+                      child: SizedBox(
+                        child: CircularProgressIndicator(
+                          strokeWidth: 2.0,
+                          valueColor:
+                              AlwaysStoppedAnimation<Color>(kColorPrimary),
+                        ),
                       ),
-                      color: kColorPrimary,
                     ),
-                  ),
-                  buildSocialLoginButton(context,
-                      icon: Icon(
-                        Icons.phone_android_outlined,
-                        color: Colors.white,
-                      ),
-                      text: 'Continue with phone number',
-                      onPressed: () {}),
-                  buildSocialLoginButton(context,
-                      icon: Image.asset(
-                        'assets/images/google-logo.png',
-                      ),
-                      text: 'Continue with Google',
-                      onPressed: () {}),
-                  buildSocialLoginButton(context,
-                      icon: Image.asset(
-                        'assets/images/facebook-logo.png',
-                        color: Color(0xFF1748BB),
-                      ),
-                      text: 'Continue with Facebook',
-                      onPressed: () {}),
-                  buildSocialLoginButton(context,
-                      icon: null, text: 'Log in', onPressed: () {}),
-                ],
-              ),
-            )
+                  )
+                : SizedBox.shrink()
           ],
         ),
       ),
+    );
+  }
+
+  Padding _buildInteractionScreen(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          const SizedBox(
+            height: 100.0,
+          ),
+          Image.asset(
+            'assets/images/logo.png',
+            scale: 11,
+          ),
+          Text(
+            'Let those words fly\n to your people on Konnect.',
+            textAlign: TextAlign.center,
+            style: Theme.of(context)
+                .textTheme
+                .headline5
+                .copyWith(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 10.0,
+          ),
+          Container(
+            width: double.infinity,
+            child: RaisedButton(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(25.0),
+                  side: BorderSide(color: Colors.transparent)),
+              onPressed: () {
+                print('Pressed');
+              },
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 12.0),
+                child: Text(
+                  'Sign up Free',
+                  style: Theme.of(context).textTheme.subtitle1.copyWith(
+                        color: Colors.white,
+                      ),
+                ),
+              ),
+              color: kColorPrimary,
+            ),
+          ),
+          buildSocialLoginButton(context,
+              icon: Icon(
+                Icons.phone_android_outlined,
+                color: Colors.white,
+              ),
+              text: 'Continue with phone number', onPressed: () {
+            print('Pressed');
+          }),
+          buildSocialLoginButton(context,
+              icon: Image.asset(
+                'assets/images/google-logo.png',
+              ),
+              text: 'Continue with Google', onPressed: () {
+            print('Pressed');
+          }),
+          buildSocialLoginButton(context,
+              icon: Image.asset(
+                'assets/images/facebook-logo.png',
+                color: Color(0xFF1748BB),
+              ),
+              text: 'Continue with Facebook', onPressed: () {
+            print('Pressed');
+          }),
+          buildSocialLoginButton(context, icon: null, text: 'Log in',
+              onPressed: () {
+            print('Pressed');
+          }),
+        ],
+      ),
+    );
+  }
+
+  Container _buildBackgroundScreen() {
+    return Container(
+      height: 450.0,
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              alignment: Alignment.topCenter,
+              fit: BoxFit.contain,
+              image: AssetImage('assets/images/bg_login.jpg'))),
     );
   }
 
@@ -107,25 +143,31 @@ class LoginPage extends StatelessWidget {
                     color: Colors.white,
                   ),
             )
-          : Row(
-              children: [
-                const SizedBox(
-                  width: 30.0,
-                ),
-                Container(width: 23.0, height: 23.0, child: icon),
-                const SizedBox(
-                  width: 10.0,
-                ),
-                Expanded(
-                  child: Text(
-                    text,
-                    textAlign: TextAlign.center,
-                    style: Theme.of(context).textTheme.subtitle1.copyWith(
-                          color: Colors.white,
-                        ),
+          : Container(
+              padding: const EdgeInsets.symmetric(vertical: 12.0),
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.white),
+                  borderRadius: BorderRadius.all(Radius.circular(25.0))),
+              child: Row(
+                children: [
+                  const SizedBox(
+                    width: 30.0,
                   ),
-                ),
-              ],
+                  Container(width: 23.0, height: 23.0, child: icon),
+                  const SizedBox(
+                    width: 10.0,
+                  ),
+                  Expanded(
+                    child: Text(
+                      text,
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.subtitle1.copyWith(
+                            color: Colors.white,
+                          ),
+                    ),
+                  ),
+                ],
+              ),
             ),
     );
   }
